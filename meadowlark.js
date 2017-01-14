dc4969e8ef7e5abedb3a47c00e7ca860823c81ef
wangdawei <melody.wang1984@gmail.com>
2017-01-14 13:03:59 +0800

    fff

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 var express = require('express');
-var fortune = require('./lib/fortune.js');
+var bodyParser = require('body-parser')
 var app = express();
+var formidable = require("formidable");
 
 // set up handlebars view engine
 var handlebars = require('express3-handlebars')
@@ -11,6 +12,16 @@ app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
+
+//支持POST的中间件
+app.use(bodyParser());
+//中间件对象
+// app.use(function(req,res,next){
+// 	if(! res.locals.partials) res.locals.partials = {};
+// 	// res.locals.partials.weather = getWeatherData();
+// 	res.locals.partials.twitter = '';
+// 	next();
+// });
 
 var fortuneCookies = [
 	"Conquer your fears or they will conquer you.",
@@ -23,9 +34,54 @@ var fortuneCookies = [
 app.get('/', function(req, res) {
 	res.render('home');
 });
+
+//显示头信息
+app.get('/headers',function(req,res){
+	res.set('Content-Type','text/plain');
+	var s="";
+	for(var name in req.headers){
+		s += name +": "+req.headers[name]+'\n';
+	}
+	res.send(s);
+})
+
 app.get('/about', function(req,res){
-	res.render('about', { fortune: fortune.getFortune() });
+	var randomFortune = 
+		fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
+	res.render('about', { fortune: randomFortune });
 });
+
+app.get('/thank-you',function(req,res){
+	res.render('thank-you');
+})
+
+//接受POST提交
+app.post('/post',function(req,res){
+	console.log('Received contact from ' + req.body.name + ' <'+req.body.email+'>');
+	//保存到数据库...
+	res.send('thinks');
+});
+
+//文件上传处理
+
+app.get('/contest/vacation-photo',function(req,res){
+	var now = new Date();
+	res.render('contest/vacation-photo',{
+		year : now.getFullYear(),month : now.getMonth()
+	});
+})
+
+app.post('/contest/vacation-photo/:year/:month',function(req,res){
+	var form = new formidable.IncomingForm();
+	form.parse(req,function(err,fields,files){
+		if(err) return res.redirect(303,'/error');
+		console.log('received fields:');
+		console.log(fields);
+		console.log('received files:');
+		console.log(files);
+		res.redirect(303,'/thank-you');
+	});
+})
 
 // 404 catch-all handler (middleware)
 app.use(function(req, res, next){
@@ -39,6 +95,8 @@ app.use(function(err, req, res, next){
 	res.status(500);
 	res.render('500');
 });
+
+
 
 app.listen(app.get('port'), function(){
   console.log( 'Express started on http://localhost:' + 
